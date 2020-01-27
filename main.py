d516d1d8d6175e6fb438d1b45c2bb9ac23e6bb95
Wahid Bawa <wahidbawa10@gmail.com>
2020-01-27 17:38:32 -0500

    simplified things

--- main.py
+++ main.py
@@ -7,21 +7,25 @@ def convert(word):
 	global convertedSentence, alphabet
 	converted = ""
 	doubleT = doubleT_Presence = th_Presence = False
+
 	for i in range(len(word)):
 		if doubleT or th_Presence:
 			doubleT = th_Presence = False
 			continue
-		elif ((word[i] == "L" or word[i] == "l") and not doubleT_Presence) or (word[i] == "R" or word[i] == "r"):
+		elif (word[i].lower() == "l" and not doubleT_Presence) or (word[i].lower() == "r"):
 			converted += "W" if word[i].isupper() else "w"
-		elif (word[i] == "T" or word[i] == "t") and ((word[i + (1 if i + 1 < len(word) else 0)] == "T" or word[i + (1 if i + 1 < len(word) else 0)] == "t")):
+		elif (word[i].lower() == "t") and ((word[i + (1 if i + 1 < len(word) else 0)].lower() == "t")):
 			converted += (("D" if word[i].isupper() else "d") + ("D" if word[i + 1].isupper() else "d")) if i + 1 < len(word) else "t"
 			doubleT = doubleT_Presence = True if i + 1 < len(word) else False
-		elif (word[i] == "T" or word[i] == "t") and ((word[i + (1 if i + 1 < len(word) else 0)] == "H" or word[i + (1 if i + 1 < len(word) else 0)] == "h")):
+		elif (word[i].lower() == "t") and ((word[i + (1 if i + 1 < len(word) else 0)].lower() == "h")):
 			converted += ("F" if word[i].isupper() else "f") if i + 2 == len(word) else "t"
 			th_Presence = True if i + 2 == len(word) else False
 		else:
 			converted += word[i]
-	convertedSentence += ((converted[0] + "-" + converted[0:]) if (rand(1, 10) == 1 and converted[0] in alphabet) else converted) + " "
+	if word[0] != ":" or word[-1] != ":":
+		convertedSentence += ((converted[0] + "-" + converted[0:]) if (rand(1, 10) == 1 and converted[0] in alphabet) else converted) + " "
+	else:
+		convertedSentence += word + " "
 
 while True: # TURN THIS TO TRUE
 	option = int(input("1. String\n2. Text File\nEnter Option here: "))
